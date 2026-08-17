d342a1a4de2dffb548fb9e6b3bbb26a2d1d51a22
misiektoja <misiektoja-github@rm-rf.ninja>
2026-08-17 19:13:06 +0200

    chore(api): relicense cmpissuer_types.go under Apache-2.0

--- api/v1alpha1/cmpissuer_types.go
+++ api/v1alpha1/cmpissuer_types.go
@@ -1,16 +1,19 @@
 /*
 Copyright 2026 The cmp-issuer Authors.
 
-SPDX-License-Identifier: GPL-3.0-only
+SPDX-License-Identifier: Apache-2.0
 
-This file is part of cmp-issuer.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-cmp-issuer is free software: you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free Software
-Foundation, version 3.
+    http://www.apache.org/licenses/LICENSE-2.0
 
-cmp-issuer is distributed in the hope that it will be useful but WITHOUT ANY
-WARRANTY. See the GNU General Public License for more details.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 
 package v1alpha1
