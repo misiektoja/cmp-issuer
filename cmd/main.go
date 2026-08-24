1fb743b8ac4e652cf1de43b644e9ad19287fef3e
misiektoja <misiektoja-github@rm-rf.ninja>
2026-08-24 19:43:29 +0200

    feat(cmd): register cert-manager Certificate resources
    Signed-off-by: misiektoja <misiektoja-github@rm-rf.ninja>

--- cmd/main.go
+++ cmd/main.go
@@ -25,6 +25,7 @@ import (
 	"fmt"
 	"os"
 
+	certmanagerv1 "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
@@ -64,6 +65,7 @@ type runConfiguration struct {
 // init registers Kubernetes and cmp-issuer APIs.
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	utilruntime.Must(certmanagerv1.AddToScheme(scheme))
 	utilruntime.Must(cmpv1alpha1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
